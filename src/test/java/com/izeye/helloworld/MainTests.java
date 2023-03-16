af411784d6d2b7c1065fc36e306d25655415c4fe
Johnny Lim <izeye@naver.com>
2023-03-16 12:43:33 +0900

    Add hello world for JaCoCo Gradle plugin

---
+++ src/test/java/com/izeye/helloworld/MainTests.java
@@ -0,0 +1,17 @@
+package com.izeye.helloworld;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Dummy tests.
+ *
+ * @author Johnny Lim
+ */
+class MainTests {
+
+    @Test
+    void test() {
+        Main.main(new String[0]);
+    }
+
+}
